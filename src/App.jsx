54d618aba433289e9d5dc24468a671f026302ebe
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-04-28 22:09:00 +0200

    Update App.jsx to use react-router-dom for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import HeaderBunner from './components/HeaderBunner/HeaderBunner'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Activities from './Pages/Activities/Activities';
+import Events from './Pages/Events/Events';
+import Hub from './Pages/Hub/Hub';
+import BlogPost from './components/BlogPost/BlogPost';
 
 function App() {
- 
   return (
-    <>
-    <HeaderBunner/>
-      <h1>Hello World</h1>
-    </>
-  )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/who-we-are" element={<About/>} />
+        <Route path="/activities" element={<Activities/>} />
+        <Route path="/outreachEvents" element={<Events/>} />
+        <Route path="/information-hub" element={<Hub/>} />
+        <Route path="/blog-post/:id" element={<BlogPost/>} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
