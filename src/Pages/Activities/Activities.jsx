5ff19c0490c8f6e0c4c30253372bbb6531ea7db9
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-04-30 10:00:02 +0200

    Refactor Activities.jsx to import useEffect and useState directly from 'react'

--- src/Pages/Activities/Activities.jsx
+++ src/Pages/Activities/Activities.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { BeatLoader } from 'react-spinners';
 import closeup from '../../assets/closeup.jpg';
