f4a28814204257c0d4d3756e61a6a5ec63a5c01f
Your felixaveiro <bikofelix2020@gmail.com>
2024-05-07 00:51:17 +0200

    editing and updating new page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Activities from './Pages/Activities/Activities';
 import Events from './Pages/Events/Events';
 import Hub from './Pages/Hub/Hub';
 import BlogPost from './components/BlogPost/BlogPost';
+import Capacity from '../src/Pages/Capacity/Capacity';
+import Youth from '../src/Pages/Youth/Youth';
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
         <Route path="/outreachEvents" element={<Events/>} />
         <Route path="/information-hub" element={<Hub/>} />
         <Route path="/blog-post/:id" element={<BlogPost/>} />
+        <Route path="/Capacity" element={<Capacity/>} />
+        <Route path="/Youth" element={<Youth/>} />
       </Routes>
       <Footer />
     </Router>
