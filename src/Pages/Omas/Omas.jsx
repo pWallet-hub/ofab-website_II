0966aec17bd810ecee1860aaedc4c78df88cb9e5
Your felixaveiro <bikofelix2020@gmail.com>
2024-06-12 19:28:34 +0200

    change and edit of website

--- src/Pages/Omas/Omas.jsx
+++ src/Pages/Omas/Omas.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Omas.css';
 import award from '../../assets/_72A2638.jpg';
+import award2 from '../../assets/_72A2747.jpg';
+import award3 from '../../assets/_72A2607.jpg';
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
+
 
 function Omas() {
     return (
@@ -15,9 +20,12 @@ function Omas() {
                     Annually, the OMAs recognize the critical roles played by the media in promoting constructive dialogue on modern biotechnology through responsible, professional, ethical, and effective reporting.
                     For the 2nd edition in Rwanda, the OFAB Media Awards were organized and implemented to recognize and promote Rwandan-based journalists who were reported reporting on the use, benefits, and other constructive information on agriculture biotechnology.
                 </p>
-                <h1>Specific Objectives
-                </h1>
-                <h2> The specific objectives of the Awards were to:</h2>
+                <div className='mission1'>
+                    <h1>Mission:</h1>
+                    <p>OMA 2024 aims to recognize and promote Rwandan-based journalists who are creating and reporting on the use, benefits, and other constructive information on agricultural biotechnology. The awards seek to highlight the critical roles played by the media in promoting constructive dialogue on modern biotechnology through responsible, professional, ethical, and effective reporting.</p>
+                </div>
+                <h1>Specific Objectives</h1>
+                <h2>The specific objectives of the Awards were to:</h2>
                 <div className='objectives'>
                     <ul>
                         <li>Promote excellence in science journalism and appreciate the contribution
@@ -29,53 +37,82 @@ function Omas() {
                             professional, ethical, and effective reporting.
                         </li>
                     </ul>
+                    <div className='award-eligibility'>
+                        <h1>Eligibility</h1>
+                        <p>Rwandan journalists who have been reporting on agricultural biotechnology and other important stories in the area of science, technology, and innovation.</p>
+                        <h3>Entry period</h3>
+                        <p>The entry period is for one year, from 31st August 2023 to 31st August 2024</p>
+                    </div>
                     <div className='award-categories'>
-                        <h1>
-                            Categories for the Award and Contestants
-                        </h1>
-                        <p>Thirty-seven contestants from different media houses were applied in this competition.
-                        </p>
-                        <h3>The categories of the Awards were:
-                        </h3>
+                        <h1>Categories for the Award and Contestants</h1>
+                        <p>Thirty-seven contestants from different media houses applied in this competition.</p>
+                        <h3>The categories of the Awards were:</h3>
                         <ul>
-                            <li>Print Media and Online Media (combined)
-                            </li>
+                            <li>Print Media and Online Media (combined)</li>
                             <li>Radio</li>
                             <li>Television</li>
-
                         </ul>
-                        <p>Then, the overall winner from either category and combined the award for his/ her category and the overall award.
-                        </p>
-
+                        <p>Then, the overall winner from either category combined the award for his/her category and the overall award.</p>
                     </div>
                 </div>
-
-            </div>
-            <div className='participants'>
-                <h1>Participants
-                </h1>
-                <p>The awards event was attended by different stakeholders.
-                 </p>
-
+                <div className='participants'>
+                    <h1>Participants</h1>
+                    <p>The awards event was attended by different stakeholders.</p>
+                    <div className='participants-list'>
+                        <ul>
+                            <li>Agricultural Biotechnology sector players</li>
+                            <li>Central government officials (MINAGRI, MoE, RAB, REMA, etc)</li>
+                            <li>Journalists/Media practitioners (mainly agricultural biotechnology)</li>
+                        </ul>
+                    </div>
+                    <p>Participants were limited to seventy attendees. Participants were encouraged to share their experiences in the Agriculture Biotechnology story</p>
+                </div>
             </div>
 
             <div className='awards-content'>
                 <div>
                     <img src={award} alt="Award" />
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam pariatur eligendi dolores, molestias incidunt
-                        ea minima natus nobis dolorum! Eaque ea neque eum provident voluptatum enim ab similique delectus saepe.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam pariatur eligendi dolores, molestias incidunt ea minima natus nobis dolorum! Eaque ea neque eum provident voluptatum enim ab similique delectus saepe.</p>
                 </div>
                 <div>
-                    <img src={award} alt="Award" />
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam pariatur eligendi dolores, molestias incidunt
-                        ea minima natus nobis dolorum! Eaque ea neque eum provident voluptatum enim ab similique delectus saepe.</p>
+                    <img src={award2} alt="Award" />
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam pariatur eligendi dolores, molestias incidunt ea minima natus nobis dolorum! Eaque ea neque eum provident voluptatum enim ab similique delectus saepe.</p>
                 </div>
                 <div>
-                    <img src={award} alt="Award" />
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam pariatur eligendi dolores, molestias incidunt
-                        ea minima natus nobis dolorum! Eaque ea neque eum provident voluptatum enim ab similique delectus saepe.</p>
+                    <img src={award3} alt="Award" />
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam pariatur eligendi dolores, molestias incidunt ea minima natus nobis dolorum! Eaque ea neque eum provident voluptatum enim ab similique delectus saepe.</p>
                 </div>
             </div>
+
+            <div className='submission'>
+                <div className='submission-guidelines'>
+                    <h2>Submission Guidelines :</h2>
+                    <h4>Contestant journalists are encouraged to submit their entries by sharing:</h4>
+                    <p>Links to published articles. The URL should be from a media house and not YouTube and SoundCloud links to TV and Radio stories respectively; and, Links to 3 of their best self-appraised articles or stories</p>
+                </div>
+                <div className='submission-deadline'>
+                    <h2>Submission Deadline</h2>
+                    <p>Entries should be submitted by Friday, 2nd September 2024, using the following OFAB Rwanda OMA 2024 submission link</p>
+                </div>
+            </div>
+            <div className='how-to-apply'>
+                <div className='how-to-apply-content'>
+                    <h2>How Apply</h2>
+                    <Link to="https://docs.google.com/forms/d/e/1FAIpQLSe9x9CS2706-3oEK46sLnVw02RdpqUU0sGpTtkyft4r7mXatg/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer">
+      <button>
+        Apply <FaArrowUpRightFromSquare className='icon21' />
+      </button>
+    </Link>
+                </div>
+                <p>Rrwanda interest in applying for the OFAB Media Awards 2024 can access the application link on OFAB Rwanda website and other social media platforms of forums.</p>
+
+                <p>We look forward to receiving your submission and celebrating the outstanding contributions of Rwanda  journalists in promoting informed dialogue on agricultural biotechnology</p>
+
+                <p>for more information, please contact:</p>
+                <p>OFAB Rwanda Secretary</p>
+                <p>Email : ofabrwanda@gmail.com</p>
+            </div>
+
         </div>
     );
 }
