f51bb0fa7b932fc42c34e50ce911e3af5710b328
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-06-16 11:10:16 +0200

    chore: Update vite.config.js to lazy load TwitterTimelineEmbed component and optimize dependencies
    This commit updates the vite.config.js file to include lazy loading for the TwitterTimelineEmbed component using React's lazy and Suspense. It also optimizes the dependencies by including the react-twitter-embed package in the optimizeDeps configuration. These changes aim to improve the page load performance and overall application loading time.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    commonjs({
+      include: /node_modules/,
+      requireReturnsDefault: 'auto',
+    })
+  ],
   optimizeDeps: {
     include: ['react-twitter-embed']
   },
   build: {
     commonjsOptions: {
       include: [/react-twitter-embed/, /node_modules/]
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-twitter-embed': ['react-twitter-embed']
+        }
+      }
     }
   }
 });
