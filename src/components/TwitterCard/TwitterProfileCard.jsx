bdb21da0d3efad80367b8275124d6fe5400ea3ff
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-06-16 10:59:28 +0200

    chore: Lazy load TwitterTimelineEmbed component in TwitterProfileCard
    This commit updates the TwitterProfileCard component to lazy load the TwitterTimelineEmbed component using React's lazy and Suspense. This change improves the page load performance by deferring the loading of the Twitter timeline until it is actually needed.

--- src/components/TwitterCard/TwitterProfileCard.jsx
+++ src/components/TwitterCard/TwitterProfileCard.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import { TwitterTimelineEmbed } from 'react-twitter-embed';
+import React, { lazy, Suspense } from 'react';
+
+const TwitterTimelineEmbed = lazy(() => import('react-twitter-embed').then(module => ({ default: module.TwitterTimelineEmbed })));
 
 const TwitterProfileCard = ({ username }) => {
   return (
     <div style={{ width: '300px', margin: '20px auto' }}>
-      <TwitterTimelineEmbed
-        sourceType="profile"
-        screenName={username}
-        options={{height: 400}}
-      />
+      <Suspense fallback={<div>Loading Twitter timeline...</div>}>
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName={username}
+          options={{height: 400}}
+        />
+      </Suspense>
     </div>
   );
 };
