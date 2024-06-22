5dc78869b9e2fc4f85523ddaf5e49748c1106133
Your felixaveiro <bikofelix2020@gmail.com>
2024-06-22 16:46:36 +0200

    change of who we are page

--- src/components/TeamCard/TeamCard.jsx
+++ src/components/TeamCard/TeamCard.jsx
@@ -1,11 +1,12 @@
 import './TeamCard.css';
 
-const TeamCard = ({ name, role, bio, image }) => {
+const TeamCard = ({ name, Institution, role, bio, image }) => {
   return (
     <div className="team-card">
       <img src={image} alt={name} className="team-card-image" />
       <div className="team-card-content">
         <h3 className="team-card-name">{name}</h3>
+        <p className="team-card-institution">{Institution}</p>
         <p className="team-card-role">{role}</p>
         <p className="team-card-bio">{bio}</p>
       </div>
