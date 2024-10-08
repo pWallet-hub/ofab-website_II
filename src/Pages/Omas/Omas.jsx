25580dbf286dcfa2808b7128f512c22ebc5d8ee7
Your felixaveiro <bikofelix2020@gmail.com>
2024-10-08 02:19:22 +0200

    change

--- src/Pages/Omas/Omas.jsx
+++ src/Pages/Omas/Omas.jsx
@@ -4,6 +4,12 @@ import './Omas.css';
 import award from '../../assets/_72A2638.jpg';
 import award2 from '../../assets/_72A2747.jpg';
 import award3 from '../../assets/_72A2607.jpg';
+import eventimage from '../../assets/shitjob.jpg';
+import ofab from '../../assets/ofab-zerobg.png';
+import breakimage from '../../assets/break.jpg';
+import rab from '../../assets/Rab.jpg';
+import alliance from '../../assets/alliance.jpg';
+import rmc from '../../assets/rmc.png';
 import { FaArrowUpRightFromSquare } from "react-icons/fa6";
 import '../Activities/Activities.css'
 
@@ -12,22 +18,60 @@ function Omas() {
     return (
         <div className='awards-container'>
             <div className='active-hub'>
-        <div className='active-content'>
-          <img className='active-img' src={award} alt="Description of image" />
-          <h3>ACTIVITIES</h3>
-        </div>
-      </div>
+                <div className='active-content'>
+                    <img className='active-img' src={eventimage} alt="Description of image" />
+                    {/* <h3>ACTIVITIES</h3> */}
+                </div>
+                <div className='welcome'>
+                    <img src={ofab} alt="ofab logo" />
+                    <div className='welcome-content'>
+                        <h1>Welcome to the 3rd Edition of the OFAB <br />
+                            Media Awards (OMA) 2024</h1>
+                        <h4>Celebrating Excellence in Agricultural Journalism!</h4>
+                    </div>
+                </div>
+                <div className='breake'>
+                    <img src={breakimage} alt="" />
+                </div>
+            </div>
 
             <div className='awards-content1'>
                 <h1>OFAB Media Awards</h1>
-                <p>Introduction
-                    Since its global inception in September 2006, the Open Forum on Agricultural Biotechnology in Africa (OFAB) project continues to enhance knowledge-sharing and awareness on agricultural biotechnology in Africa and facilitates quality engagement on safety and benefits of modern agricultural biotechnology among stakeholders.
-                    The Project, which is a partnership between the African Agricultural Technology Foundation (AATF) and other like-minded organizations, is implemented at the country level mostly through government bodies forming OFAB chapters serving as secretariats in those countries. Currently, OFAB is operational in ten countries namely, Kenya, Uganda, Tanzania, Ethiopia, Nigeria, Ghana, Burkina Faso, Rwanda, Mozambique, and Malawi.
+                <p>Ladies and gentlemen, distinguished guests, and champions of agricultural innovation, we are
+                    thrilled to present to you the 3rd Edition of the OFAB Media Awards (OMA) 2024, an event
+                    designed to honor and elevate the voices of Rwandan journalists who have excelled in reporting on the transformative power of agricultural biotechnology.
                 </p>
-                <p>The OFAB Media Awards (OMAs) are aimed at recognizing exemplary journalism that exhibits best practices in credible science reporting that is crucial to better public understanding and acceptance of sciences, technologies, and innovations that are needed to transform African agriculture for food security, sustainable development, and poverty eradication.
-                    Annually, the OMAs recognize the critical roles played by the media in promoting constructive dialogue on modern biotechnology through responsible, professional, ethical, and effective reporting.
-                    For the 2nd edition in Rwanda, the OFAB Media Awards were organized and implemented to recognize and promote Rwandan-based journalists who were reported reporting on the use, benefits, and other constructive information on agriculture biotechnology.
+                <p>As you know, journalism plays a pivotal role in shaping public understanding and driving conversations on key issues like food security, sustainable farming, and the technological
+                    advancements critical to Rwanda’s agricultural development. These awards recognize credible
+                    and ethical journalism that fosters constructive dialogue, promotes science-based reporting,
+                    and ultimately supports Rwanda’s mission to build a thriving, tech-driven agricultural sector.
                 </p>
+                <p>This invitation-only event brings together Rwandan media professionals, business leaders,
+                    government officials, and civil society stakeholders. Together, we celebrate the exceptional
+                    contributions of journalists who have reported insightful, accurate, and impactful stories on
+                    the application of modern biotechnology in agriculture.</p>
+                <img src={award} alt="" />
+                <div className='change'>
+                    <h2>since 2021</h2>
+                    <h1>OFAB Media Awards (OMAs)</h1>
+                    <p>have been implemented under a consortium group consisting</p>
+                    <div className='images'>
+                        <img src={rab} alt="" />
+                        <img src={rmc} alt="" />
+                        <img src={alliance} alt="" />
+                    </div>
+
+                </div>
+                <p>This partnership was established to promote science-based journalism and enhance the capacity of
+                    Rwandan media in reporting on agricultural biotechnology. Through the OMAs, the consortium has
+                    worked collaboratively to foster responsible, accurate, and ethical reporting, recognizing journalists who
+                    contribute to advancing public understanding of agricultural innovations. </p>
+                <div className='image-content'>
+                    <img src={award2} alt="" />
+                    <p>By uniting key stakeholders from media, science, and agriculture, the OMAs aim to elevate journalism that informs
+                        policy discussions, educates the public, and supports Rwanda's goals for sustainable agricultural development.</p>
+
+                </div>
                 <div className='mission1'>
                     <h1>Mission:</h1>
                     <p>OMA 2024 aims to recognize and promote Rwandan-based journalists who are creating and reporting on the use, benefits, and other constructive information on agricultural biotechnology. The awards seek to highlight the critical roles played by the media in promoting constructive dialogue on modern biotechnology through responsible, professional, ethical, and effective reporting.</p>
@@ -86,8 +130,8 @@ function Omas() {
                     <img src={award2} alt="Award" />
                     <h3>Mr. Emmanuel Ntirenganya</h3>
                     <p>Mr. Emmanuel Ntirenganya clinched the top honor as the inaugural winner of the OFAB Media Awards in 2022, excelling in the distinguished writing category.
-                    excelling in the distinguished writing category with the "How agricultural biotechnology could boost food security"
-.                    </p>
+                        excelling in the distinguished writing category with the "How agricultural biotechnology could boost food security"
+                        .                    </p>
                 </div>
                 <div>
                     <img src={award3} alt="Award" />
@@ -110,10 +154,10 @@ function Omas() {
                 <div className='how-to-apply-content'>
                     <p className='title-how'>How to Apply</p>
                     <Link to="https://docs.google.com/forms/d/e/1FAIpQLSe9x9CS2706-3oEK46sLnVw02RdpqUU0sGpTtkyft4r7mXatg/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer">
-      <button>
-        Apply <FaArrowUpRightFromSquare className='icon21' />
-      </button>
-    </Link>
+                        <button>
+                            Apply <FaArrowUpRightFromSquare className='icon21' />
+                        </button>
+                    </Link>
                 </div>
                 <p>Rwanda interest in applying for the OFAB Media Awards 2024 can access the application link on OFAB Rwanda website and other social media platforms of forums.</p>
 
